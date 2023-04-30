601546e985b139c5360f454319c8de7c1d775954
KolosovAA <andre.e.04@gmail.com>
2023-04-30 17:02:48 +0300

    Initial commit

---
+++ server/src/main/java/Main.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(34555)) {
+            while (true) {
+                try (
+                        Socket clientSocket = serverSocket.accept();
+                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                ) {
+                    out.println("Write your name?");
+                    System.out.println("New connection accepted.");
+                    final String name = in.readLine();
+                    out.println(String.format("Hi %s. Are you child? (yes/no)", name, clientSocket.getPort()));
+                    final String child = in.readLine();
+                    if (child.equals("yes")) {
+                        out.println(String.format("Welcome to the kids area, %s! Let's play!", name));
+                    } else {
+                        out.println(String.format("Welcome to the adult zone, %s! Have a good rest, or a good working day!", name));
+                    }
+                    out.println("Bye. hope we had a great time.(yes/no)");
+                    final String greatTime = in.readLine();
+                    if (child.equals("yes")) {
+                        out.println(String.format("see you, %s", name));
+                    } else {
+                        out.println(String.format("see you, %s. hope next time will be better", name));
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ user/src/main/java/Main.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String host = "netology.homework";
+        int port = 34555;
+
+        try (
+                Socket clientSocket = new Socket(host, port);
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+        ) {
+            String yourName = in.readLine();
+            System.out.println(yourName);
+
+            Scanner scanner = new Scanner(System.in);
+            String name = scanner.nextLine();
+            out.println(name);
+            String resp = in.readLine();
+            System.out.println(resp);
+
+            String yOrN = scanner.nextLine();
+            out.println(yOrN);
+            String child = in.readLine();
+            System.out.println(child);
+
+            String byServer = in.readLine();
+            System.out.println(byServer);
+
+            String yesOrNo = scanner.nextLine();
+            out.println(yesOrNo);
+            String seeYou = in.readLine();
+            System.out.println(seeYou);
+
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
